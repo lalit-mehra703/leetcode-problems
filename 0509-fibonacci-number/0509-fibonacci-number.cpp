2426331e80b02f1f2858b40751c4f05d368c2d60
Lalit Mohan Mehra <lokeshmehra7039@gmail.com>
2026-03-07 22:34:37 +0530

    Time: 0 ms (100%), Space: 7.8 MB (64.26%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if( n == 0 || n == 1){
+            return n;
+        }
+        int ans = 0 ; 
+        int prev1 = 1, prev2 = 0;
+        for(int i = 2; i <= n ; i++){
+            ans = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = ans;
+        }
+        return ans;
+    }
+};
