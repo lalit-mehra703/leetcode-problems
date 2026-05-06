64fa6f1072eafef0ef7d940fecd8109c860104fc
Lalit Mohan Mehra <lokeshmehra7039@gmail.com>
2026-05-06 10:20:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (2.85%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -3,18 +3,17 @@ public:
     int balancedStringSplit(string s) {
         int countR = 0;
         int countL = 0;
-        int ans = 1;
+        int ans = 0;
 
         for(int i = 0 ; i < s.length(); i++){
-            if(countR != 0 && countR == countL){
-                ans++;
-                countR = 0;
-                countL = 0;
-            }
             if(s[i] == 'R'){
                 countR++;
             }else{
                 countL++;
+            }
+
+            if(countR == countL){
+                ans++;
             }
         }
         return ans;
